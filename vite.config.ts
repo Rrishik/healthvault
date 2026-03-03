877cff04b366e373f3ff8923beb2beedcab03afb
Rrishik <ramenarishik@gmail.com>
2026-03-03 23:41:09 +0530

    Fix base path for custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/healthvault/',
+  base: '/',
   server: {
     port: 5173,
     strictPort: true,
